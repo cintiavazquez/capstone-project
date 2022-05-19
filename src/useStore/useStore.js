0c25945ddd41c95f6d2ea3595717f7725e78108f
Cintia Vazquez <vazquezalvarezcintia@gmail.com>
2022-05-19 10:23:36 +0200

    local storage with persist

--- src/useStore/useStore.js
+++ src/useStore/useStore.js
@@ -1,45 +1,48 @@
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 import { nanoid } from 'nanoid';
 
-const useStore = create(set => ({
-	modalState: false,
-	modalHide: () => {
-		set({ modalState: false });
-	},
-	modalShow: () => {
-		set({ modalState: true });
-	},
-	reviews: [
-		{
-			id: nanoid(),
-			name: 'Vegan yoghurt',
-			rating: 'good',
-			comment: 'I liked this product',
-			location: 'Edeka Hamburg',
+const useStore = create(
+	persist(set => ({
+		modalState: false,
+		modalHide: () => {
+			set({ modalState: false });
 		},
-		{
-			id: nanoid(),
-			name: 'Mango ice cream',
-			rating: 'bad',
-			comment: "I didn't like this product",
-			location: 'Eisdiele Hamburg',
+		modalShow: () => {
+			set({ modalState: true });
 		},
-	],
-	addReview: data => {
-		set(state => {
-			return {
-				reviews: [
-					...state.reviews,
-					{
-						id: nanoid(),
-						name: data.name,
-						rating: data.rating,
-						comment: data.comment,
-						location: data.location,
-					},
-				],
-			};
-		});
-	},
-}));
+		reviews: [
+			{
+				id: nanoid(),
+				name: 'Vegan yoghurt',
+				rating: 'good',
+				comment: 'I liked this product',
+				location: 'Edeka Hamburg',
+			},
+			{
+				id: nanoid(),
+				name: 'Mango ice cream',
+				rating: 'bad',
+				comment: "I didn't like this product",
+				location: 'Eisdiele Hamburg',
+			},
+		],
+		addReview: data => {
+			set(state => {
+				return {
+					reviews: [
+						...state.reviews,
+						{
+							id: nanoid(),
+							name: data.name,
+							rating: data.rating,
+							comment: data.comment,
+							location: data.location,
+						},
+					],
+				};
+			});
+		},
+	}))
+);
 export default useStore;
