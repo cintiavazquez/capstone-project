d5ad12b069f60bf0eec30f617d4faf15983103db
Cintia Vazquez <vazquezalvarezcintia@gmail.com>
2022-05-29 22:18:14 +0200

    style nav

--- src/UI/Navigation.styled.js
+++ src/UI/Navigation.styled.js
@@ -5,9 +5,7 @@ export const Navigation = styled.nav`
 	bottom: 0;
 	right: 0;
 	left: 0;
-	display: flex;
-	justify-content: space-around;
 	width: 100%;
-	background: orangered;
-	padding: 5px;
+	height: 8vh;
+	z-index: 3;
 `;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import { Button } from '../../UI/Button.styled';
+import Div from '../../UI/Div.styled';
+import DivFlex from '../../UI/DivFlex.styled';
 import { Navigation } from '../../UI/Navigation.styled';
 import SVGIcon from '../../UI/SVGIcon';
 
@@ -7,21 +9,41 @@ export default function Navbar() {
 	return (
 		<footer>
 			<Navigation>
-				<Link passHref href="/">
-					<Button type="button" variant="nav">
-						<SVGIcon variant="home" color="white" />
-					</Button>
-				</Link>
-				<Link passHref href="/post">
-					<Button type="button" variant="nav">
-						<SVGIcon variant="post" color="white" />
-					</Button>
-				</Link>
-				<Link passHref href="/profile">
-					<Button type="button" variant="nav">
-						<SVGIcon variant="profile" color="white" />
-					</Button>
-				</Link>
+				<Div
+					width="100%"
+					height="100%"
+					background="linear-gradient(370deg, rgba(148,126,163,0.97) 33%, rgba(212,233,199,0.88) 49%, rgba(196,196,196,0) 100%);"
+				>
+					<Div
+						width="100%"
+						height="100%"
+						background="linear-gradient(144deg, rgba(142,163,126,0.25) 31%, rgba(144,216,190,0.36) 38%, rgba(250,252,158,1) 61%, rgba(187,244,214,0.5) 66%, rgba(149,192,155,0.69) 100%)"
+					>
+						<DivFlex
+							borderRadius="none"
+							justifyContent="space-around"
+							width="100%"
+							height="100%"
+							background="linear-gradient(31deg, rgba(148,126,163,1) 5%, rgba(148,126,163,0.58) 29%, rgba(196,196,196,0) 100%);"
+						>
+							<Link passHref href="/">
+								<Button type="button" variant="nav">
+									<SVGIcon variant="home" color="var(--dark-lilac)" />
+								</Button>
+							</Link>
+							<Link passHref href="/post">
+								<Button type="button" variant="nav">
+									<SVGIcon variant="post" color="var(--dark-lilac)" />
+								</Button>
+							</Link>
+							<Link passHref href="/profile">
+								<Button type="button" variant="nav">
+									<SVGIcon variant="profile" color="var(--dark-lilac)" />
+								</Button>
+							</Link>
+						</DivFlex>
+					</Div>
+				</Div>
 			</Navigation>
 		</footer>
 	);
