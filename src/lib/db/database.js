4de4ca50c99273c6e165ce0a87e2edfc2719b5e9
Cintia Vazquez <vazquezalvarezcintia@gmail.com>
2022-06-06 10:27:48 +0200

    modify static json, add mongoose connection

---
+++ src/lib/db/database.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+//const url = process.env.DB_CONNECTION;
+const url = 'mongodb://localhost:27017/vgo';
+
+export async function dbConnect() {
+	try {
+		await mongoose.connect(url);
+		console.log('Connected to MongoDB');
+	} catch (error) {
+		console.error('ERROR, could not connect', error.message);
+	}
+}
