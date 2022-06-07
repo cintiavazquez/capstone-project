1b2728a1bd3d00fd786e60f7a7e9e1660e156638
Cintia Vazquez <vazquezalvarezcintia@gmail.com>
2022-06-07 22:21:24 +0200

    fetch reviews in post page

--- pages/post.js
+++ pages/post.js
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
+
 import Form from '../src/components/Form/Form';
 import Modal from '../src/components/Form/Modal';
+import useStore from '../src/useStore/useStore';
 
 export default function Post() {
+	useEffect(() => {
+		useStore.getState().fetchReviews();
+	}, []);
 	return (
 		<>
 			<Modal />
